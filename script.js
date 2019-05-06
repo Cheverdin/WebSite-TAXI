4c4ab2c555e4a249dc1f957a7f9c82d905287dec
Vlad Cheverdin <vladislavcheverdin@gmail.com>
2019-05-06 20:09:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+﻿
+
+menu.onclick = function myfunction() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'navbar') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navbar';
+  }
+}
+
+
+/* Кнопка */
+
+var form_btn = document.getElementsByClassName("icon_btn");
+
+
+
+ /* РАБОЧИЙ КОД */
+
+
+    var acc = document.getElementsByClassName("accardion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var accardion_cont = this.nextElementSibling;
+    if (accardion_cont.style.maxHeight){
+        accardion_cont.style.maxHeight = null;
+    } else {
+        accardion_cont.style.maxHeight =accardion_cont.scrollHeight + "px";
+    } 
+  });
+}
+ 
+
